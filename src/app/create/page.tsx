52fea1531fbd60fd6fca0999eaa4d34fdae08d58
yatakei <k.yatagai1111@gmail.com>
2025-05-25 11:31:23 +0900

    firebase接続確認

---
+++ src/app/create/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import CreateButton from '@/components/CreateButton';
+import React from 'react';
+
+const login = () => {
+  return (
+    <div>
+      ddddd
+      <CreateButton />
+    </div>
+  );
+};
+
+export default login;
---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,12 @@
+import { auth } from '@/lib/firebase/firebase';
+import React from 'react'
+
+const UserInfo = () => {
+
+
+  return (
+    <div>ddd</div>
+  )
+}
+
+export default UserInfo
